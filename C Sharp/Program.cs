a5a6559165d2aece0e786b8771113583d70f86ad
Nathan-Yinka <oludarenathaniel@gmail.com>
2024-08-20 09:00:46 +0100

    TryParse compleme

--- C Sharp/Program.cs
+++ C Sharp/Program.cs
@@ -1,35 +1,11 @@
-﻿using System.Globalization;
+﻿Console.Write("Enter a value: ");
+string valueString = Console.ReadLine();
 
-double value = 1000D / 12.34D;
+int num;
+bool success = int.TryParse(valueString, out num);
 
-Console.WriteLine(value);
-Console.WriteLine(string.Format("{0:0.0}", value));
-Console.WriteLine(string.Format("{0:0.00}", value));
-Console.WriteLine(string.Format("{0:0.000}", value));
-Console.WriteLine(string.Format("{0:F1}", value));
+string res = success ? $"a valid int was entered {num}" : "the int was not valid";
 
-
-double money = -10D / 3D;
-Console.WriteLine(string.Format("${0:0.00}", money));
-Console.WriteLine(money.ToString("C"));
-Console.WriteLine(money.ToString("C0"));
-Console.WriteLine(money.ToString("C1"));
-Console.WriteLine(money.ToString("C2"));
-
-
-
-
-//currency formatting
-Console.WriteLine(money.ToString("C",CultureInfo.CurrentCulture)); // this will give us the default currency formatting
-
-Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-GB")));
-Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
-Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-AU")));
-
-Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-GB")));
-Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
-Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-AU")));
-
-Console.WriteLine("are you mas");
+Console.WriteLine(res);
 
 Console.ReadLine();
