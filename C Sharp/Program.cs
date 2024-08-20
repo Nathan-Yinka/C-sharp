154e1252189fb43901f9e53e1d56108a52d5533c
Nathan-Yinka <oludarenathaniel@gmail.com>
2024-08-20 02:14:39 +0100

    done

--- C Sharp/Program.cs
+++ C Sharp/Program.cs
@@ -1,67 +1,35 @@
-﻿for (int i = 0; i < 5; i++){
-    Console.WriteLine("Hi");
-}
+﻿using System.Globalization;
+
+double value = 1000D / 12.34D;
+
+Console.WriteLine(value);
+Console.WriteLine(string.Format("{0:0.0}", value));
+Console.WriteLine(string.Format("{0:0.00}", value));
+Console.WriteLine(string.Format("{0:0.000}", value));
+Console.WriteLine(string.Format("{0:F1}", value));
 
 
-//while loop
-
-
-int j = 0;
-while (j < 5){
-    Console.WriteLine("this is the while loop");
-    j++;
-}
-
-
-// making a game guesser
-Console.Write("Enter the first number: ");
-double num1 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Enter the second number: ");
-double num2 = Convert.ToInt32(Console.ReadLine());
-
-double result = num1 * num2;
-
-double value = 0;
-
-//while (result != value)
-//{
-//    Console.Write("Enter the result of the mutiplication: ");
-//    value = Convert.ToInt32(Console.ReadLine());
-
-//    if (value != result)
-//    {
-//        Console.WriteLine ("Close but it is wrong");
-//    }
-//}
-
-//Console.WriteLine("Well done");
-
-//Console.ReadLine();
-
-/*
-// do while loop 
-
-do
-{
-    Console.Write("Enter the result of the multiplication: ");
-    value = Convert.ToInt32(Console.ReadLine());
-
-    if (value != result)
-    {
-        Console.WriteLine("Close, but it is wrong.");
-    }
-} while (result != value);
-
-Console.WriteLine("Correct! Well done.");
+double money = -10D / 3D;
+Console.WriteLine(string.Format("${0:0.00}", money));
+Console.WriteLine(money.ToString("C"));
+Console.WriteLine(money.ToString("C0"));
+Console.WriteLine(money.ToString("C1"));
+Console.WriteLine(money.ToString("C2"));
 
 
 
-*/
 
+//currency formatting
+Console.WriteLine(money.ToString("C",CultureInfo.CurrentCulture)); // this will give us the default currency formatting
 
-// condtional opaterator
+Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-GB")));
+Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-AU")));
 
-int age = 10;
+Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-GB")));
+Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-AU")));
 
-Console.WriteLine(age > 10 ? "Valid" : "Invalid");
+Console.WriteLine("are you mas");
+
+Console.ReadLine();
